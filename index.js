9a489e6b48b89848ca0fbbc20398359f7092f075
AlexandraMendiv <>
2021-08-10 15:09:27 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+//entrada
+var edad = parseInt(prompt("¿Cuál es tu edad?"));
+//proceso
+if(edad>= 18){
+  //salida1
+    console.log("Eres mayor de edad, puedes conducir");
+}else{
+  //salida2
+    console.log("Eres menor de edad, no puedes conducir");
+}
+
+
+
+
